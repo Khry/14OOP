07434fa0756910c0b0355ff41492aa31f97b4397
Khry <khrustyk@gmail.com>
2017-08-03 23:32:30 +0300

    add1

--- script/main.js
+++ script/main.js
@@ -2,54 +2,63 @@ function Casino(numberOfSlotMachines, initialAmountOfMoney) {
   if (numberOfSlotMachines <= 0 || initialAmountOfMoney <= 0) {
     return 'Casino creating error';
   }
-  this.slotMachines = [];
+  this.slotMachinesArr = [];
   var moneyInMachine = Math.floor(initialAmountOfMoney / numberOfSlotMachines);
   var moneyRemainder = initialAmountOfMoney % numberOfSlotMachines;
-  this.slotMachines[0] = new SlotMachine(moneyInMachine + moneyRemainder);
-  this.slotMachines[0].id = 0;
-  this.slotMachines[0].lucky = true;
+  this.slotMachinesArr[0] = new SlotMachine(moneyInMachine + moneyRemainder);
+  this.slotMachinesArr[0].id = 0;
+  this.slotMachinesArr[0].lucky = true;
   for (var i = 1; i < numberOfSlotMachines; i++) {
-    this.slotMachines[i] = new SlotMachine(moneyInMachine);
-    this.slotMachines[i].id = i;
+    this.slotMachinesArr[i] = new SlotMachine(moneyInMachine);
+    this.slotMachinesArr[i].id = i;
   }
   this.getAmountOfMoney = function() {
     var amount = 0;
     for (var i = 0; i < this.numberOfSlotMachines; i++) {
-      amount += this.slotMachines[i].amountOfMoneyInMachine;
+      amount += this.slotMachinesArr[i].amountOfMoneyInMachine;
     }
     return amount;
   };
   this.numberOfSlotMachines = numberOfSlotMachines;
-  this.getNumberOfSlotMachines = function() {
-    console.log(this.numberOfSlotMachines);
+  this.getNumberOfMachines = function() {
     return this.numberOfSlotMachines;
   };
-  this.addSlotMachine = function() {
+  this.addMachine = function() {
     var largest = 0;
-    for (var i = 0; i < this.slotMachines.length; i++) {
-      if (this.slotMachines[i].amountOfMoneyInMachine > largest) {
-        largest = this.slotMachines[i].amountOfMoneyInMachine;
+    for (var i = 0; i < this.slotMachinesArr.length; i++) {
+      if (this.slotMachinesArr[i].amountOfMoneyInMachine > largest) {
+        largest = this.slotMachinesArr[i].amountOfMoneyInMachine;
       }
     }
-    this.slotMachines[numberOfSlotMachines] = new SlotMachine(largest/2);
-    this.slotMachines[numberOfSlotMachines].id = numberOfSlotMachines;
+    this.slotMachinesArr[numberOfSlotMachines] = new SlotMachine(largest / 2);
+    this.slotMachinesArr[numberOfSlotMachines].id = numberOfSlotMachines;
     this.numberOfSlotMachines++;
   }
-  this.removeSlotMachine = function(id) {
-    for (var i = 0; i < this.slotMachines.length; i++) {
-      if (id === this.slotMachines[i].id) {
-        foundedSlotMachine = this.slotMachines[i];
-        this.slotMachines.splice(i, 1);
+  this.removeMachine = function(id) {
+    for (var i = 0; i < this.slotMachinesArr.length; i++) {
+      if (id === this.slotMachinesArr[i].id) {
+        foundedSlotMachine = this.slotMachinesArr[i];
+        var necessaryAmountOfMoney = Math.floor( this.slotMachinesArr[i].amountOfMoneyInMachine / (this.slotMachinesArr.length - 1));
+        console.log(necessaryAmountOfMoney);
+        var remainder = this.slotMachinesArr[i].amountOfMoneyInMachine - necessaryAmountOfMoney * (this.slotMachinesArr.length - 1);
+        console.log(remainder);
+        this.slotMachinesArr[0].amountOfMoneyInMachine += remainder;
+        for (var j = 0; j < this.slotMachinesArr.length; j++) {
+          this.slotMachinesArr[j].amountOfMoneyInMachine += necessaryAmountOfMoney;
+        }
+        this.slotMachinesArr.splice(i, 1);
         break;
       }
     }
+
     this.numberOfSlotMachines--;
+    return 'You removed slot machine';
   }
   this.takeMoneyFromCasino = function(amountOfMoney) {
     if (this.getAmountOfMoney() < amountOfMoney) {
       return 'You want too much...';
     }
-    this.slotMachines.sort(function(a, b) {
+    this.slotMachinesArr.sort(function(a, b) {
       if (a.amountOfMoneyInMachine < b.amountOfMoneyInMachine) {
         return 1;
       }
@@ -58,16 +67,17 @@ function Casino(numberOfSlotMachines, initialAmountOfMoney) {
       }
       return 0;
     });
-    var necessaryAmountOfMoney = Math.round(amountOfMoney / this.slotMachines.length);
-    var remainder = amountOfMoney - necessaryAmountOfMoney * this.slotMachines.length;
-    this.slotMachines[0].amountOfMoneyInMachine -= remainder;
-    for (var i = 0; i < this.slotMachines.length; i++) {
-      if (this.slotMachines[i].amountOfMoneyInMachine >= necessaryAmountOfMoney) {
-        this.slotMachines[i].amountOfMoneyInMachine -= necessaryAmountOfMoney;
+    var necessaryAmountOfMoney = Math.round(amountOfMoney / this.slotMachinesArr.length);
+    var remainder = amountOfMoney - necessaryAmountOfMoney * this.slotMachinesArr.length;
+    this.slotMachinesArr[0].amountOfMoneyInMachine -= remainder;
+    for (var i = 0; i < this.slotMachinesArr.length; i++) {
+      if (this.slotMachinesArr[i].amountOfMoneyInMachine >= necessaryAmountOfMoney) {
+        this.slotMachinesArr[i].amountOfMoneyInMachine -= necessaryAmountOfMoney;
       }
     }
-    return this.slotMachines;
+    return this.slotMachinesArr;
   }
+
   function SlotMachine(initialAmountOfMoneyInMachine) {
     this.lucky = false;
     this.amountOfMoneyInMachine = initialAmountOfMoneyInMachine;
@@ -76,9 +86,11 @@ function Casino(numberOfSlotMachines, initialAmountOfMoney) {
     }
     this.takeMoneyFromSlotMachine = function(amountOfMoney) {
       this.amountOfMoneyInMachine -= amountOfMoney;
+      return 'You took '+ amountOfMoney + ' from slot machine';
     }
-    this.putMoney = function(amountOfMoney) {
+    this.putMoneyInSlotMachine = function(amountOfMoney) {
       this.amountOfMoneyInMachine += amountOfMoney;
+      return 'You put '+ amountOfMoney + ' in slot machine';
     }
     this.play = function(amountOfMoney) {
       if (amountOfMoney <= 0) {
@@ -91,7 +103,7 @@ function Casino(numberOfSlotMachines, initialAmountOfMoney) {
       var stringRandomNumber = randomNumber.toString();
       if (stringRandomNumber === '777' && this.lucky) {
         this.amountOfMoneyInMachine = 0;
-        return 'You`re a lucky guy, you won '+ this.amountOfMoneyInMachine;
+        return 'You`re a lucky guy, you won ' + this.amountOfMoneyInMachine;
       }
       for (var i = 1; i < stringRandomNumber.length; i++) {
         var substrings = stringRandomNumber.split(stringRandomNumber[i]);
@@ -101,7 +113,6 @@ function Casino(numberOfSlotMachines, initialAmountOfMoney) {
             return 'You`re a lucky guy, you won ' + this.amountOfMoneyInMachine;
           } else {
             this.amountOfMoneyInMachine -= amountOfMoney * 2;
-            // Casino.amountOfMoney -= amountOfMoney * 2;
             return 'You`re a lucky guy, you won ' + amountOfMoney * 2;
           }
         } else if (substrings.length - 1 === 3) {
@@ -110,7 +121,6 @@ function Casino(numberOfSlotMachines, initialAmountOfMoney) {
             return 'You`re a lucky guy, you won ' + this.amountOfMoneyInMachine;
           } else {
             this.amountOfMoneyInMachine -= amountOfMoney * 5;
-            // Casino.amountOfMoney -= amountOfMoney * 5;
             return 'You`re a lucky guy, you won ' + amountOfMoney * 5;
           }
         } else {
@@ -127,19 +137,27 @@ var casino = new Casino(15, 100000);
 
 function demonstrate() {
   console.log(casino);
-  casino.addSlotMachine();
+  casino.addMachine();
   console.log(casino);
-  casino.getNumberOfSlotMachines();
-  console.log(casino.slotMachines[0].getAmountOfMoneyInMachine());
-  console.log(casino.slotMachines[1].getAmountOfMoneyInMachine());
-  console.log(casino.slotMachines[5].getAmountOfMoneyInMachine());
+  casino.getNumberOfMachines();
+  console.log(casino.slotMachinesArr[0].getAmountOfMoneyInMachine());
+  console.log(casino.slotMachinesArr[5].getAmountOfMoneyInMachine());
   console.log(casino.getAmountOfMoney());
-  console.log(casino.slotMachines[1].play(77));
-  console.log(casino.slotMachines[1].getAmountOfMoneyInMachine());
+  console.log(casino.slotMachinesArr[1].play(77));
+  console.log(casino.slotMachinesArr[1].getAmountOfMoneyInMachine());
   console.log(casino.getAmountOfMoney());
   console.log(casino.takeMoneyFromCasino(200000));
   console.log(casino.takeMoneyFromCasino(200));
   console.log(casino.getAmountOfMoney());
+  console.log(casino.removeMachine(5));
+  console.log(casino.getNumberOfMachines());
+  console.log(casino.getAmountOfMoney());
+  console.log(casino.slotMachinesArr[0].getAmountOfMoneyInMachine());
+  console.log(casino.slotMachinesArr[0].takeMoneyFromSlotMachine(50));
+  console.log(casino.slotMachinesArr[0].getAmountOfMoneyInMachine());
+  console.log(casino.getAmountOfMoney());
+  console.log(casino.slotMachinesArr[0].putMoneyInSlotMachine(80));
+  console.log(casino.slotMachinesArr[0].getAmountOfMoneyInMachine());
+  console.log(casino.getAmountOfMoney());
 }
-
 demonstrate();
